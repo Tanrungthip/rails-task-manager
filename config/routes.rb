a3989e81114302f85577e9dcbd4f9b6699ba8981
Rungthip Chaothayee <ttann.trc@gmail.com>
2023-02-14 15:09:28 +0900

    complete the logic but no css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Read all
+  get '/tasks', to: 'tasks#index', as: :tasks
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Create
+  # this is a form page
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # this is where the form get sent
+  post '/tasks', to: 'tasks#create'
+
+  # Update
+  # this is a form page (specific page u want to edit)
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # this is where the edit gets sent
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # Read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
